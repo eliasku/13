af4df8548688fec4a7d341a303a2f587db6a6850
eliasku <deadbabe@gmail.com>
2022-08-25 12:28:45 +0300

    vpad circles

--- src/lib/game/controls.ts
+++ src/lib/game/controls.ts
@@ -62,9 +62,10 @@ export function updateControls(player: Actor) {
         const k = 1.0 / camera.scale_;
         if (touchPadActive) {
             {
+                const control = vpad[0];
                 let dx = 0;
                 let dy = 0;
-                const pp = vpad[0].pointer_;
+                const pp = control.pointer_;
                 if (pp) {
                     dx = (pp.x_ - pp.startX_) * k;
                     dy = (pp.y_ - pp.startY_) * k;
@@ -72,13 +73,14 @@ export function updateControls(player: Actor) {
                 const len = Math.hypot(dx, dy);
                 moveX = dx;
                 moveY = dy;
-                moveFast = (len > 16) as any | 0;
-                jumpButtonDown = (len > 32) as any | 0;
+                moveFast = (len > control.r1_) as any | 0;
+                jumpButtonDown = (len > control.r2_) as any | 0;
             }
             {
+                const control = vpad[1];
                 let dx = 0;
                 let dy = 0;
-                const pp = vpad[1].pointer_;
+                const pp = control.pointer_;
                 if (pp) {
                     dx = (pp.x_ - pp.startX_) * k;
                     dy = (pp.y_ - pp.startY_) * k;
@@ -88,7 +90,7 @@ export function updateControls(player: Actor) {
                 viewY = dy;
                 lookAtX = px + dx * 3;
                 lookAtY = py + dy * 3;
-                shootButtonDown = (len > 24) as any | 0;
+                shootButtonDown = (len > control.r2_) as any | 0;
             }
             dropButton = vpad[2].pointer_ ? 1 : 0;
         }
@@ -102,11 +104,16 @@ interface VPadControl {
     b_: number;
     flags_?: number;
     pointer_?: Pointer | undefined;
+    // any len > undefined = false (undefined is NaN)
+    r1_?:number|undefined;
+    r2_?:number|undefined;
 }
 
+// TODO: positioning of controls
+// ToDO: control zone padding should include max radius
 const vpad: VPadControl[] = [
-    {l_: 0, t_: 0.5, r_: 0.5, b_: 1},
-    {l_: 0.5, t_: 0.5, r_: 1, b_: 1},
+    {l_: 0, t_: 0.5, r_: 0.5, b_: 1, r1_: 16, r2_: 32},
+    {l_: 0.5, t_: 0.5, r_: 1, b_: 1, r1_: 8, r2_: 24},
     {l_: 0.5, t_: 0, r_: 1, b_: 0.5, flags_: 1},
 ];
 let touchPadActive = false;
@@ -171,14 +178,16 @@ export function drawVirtualPad() {
         const h_ = H * (control.b_ - control.t_);
         const cx = W * control.l_ + w_ / 2;
         const cy = H * control.t_ + h_ / 2;
-        draw(img_circle_16, cx, cy, 0, 2, 2, 0x22000000);
+        draw(img_box, cx, cy, 0, w_, h_, 0x22000000);
         const pp = control.pointer_;
         if (pp) {
             if (control.flags_ & 1) {
                 draw(img_box, cx, cy, 0, w_, h_, pp ? 0x22FFFFFF : 0x22000000);
             } else {
-                draw(img_box, pp.startX_ * k, pp.startY_ * k, 0, 32, 32, 0x77FFFFFF);
-                draw(img_box, pp.startX_ * k, pp.startY_ * k, 0, 64, 64, 0x77FFFFFF);
+                const r1 = (control.r1_ / 16);
+                const r2 = (control.r2_ / 16);
+                draw(img_circle_16, pp.startX_ * k, pp.startY_ * k, 0, r1, r1, 0x77FFFFFF);
+                draw(img_circle_16, pp.startX_ * k, pp.startY_ * k, 0, r2, r2, 0x77FFFFFF);
                 draw(img_circle_16, pp.x_ * k, pp.y_ * k, 0, 1, 1, 0x77FFFFFF);
             }
         }
