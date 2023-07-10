d400a212c4e0db4c29ffe83865b62393712c1967
eliasku <deadbabe@gmail.com>
2023-07-10 11:42:04 +0300

    remove commented code

--- packages/tools/src/build.ts
+++ packages/tools/src/build.ts
@@ -7,10 +7,6 @@ prepareKeysFolder();
 prepareFolders("public");
 copyPublicAssets();
 
-// console.info("Compile typescript...");
-// const res = await executeAsync("tsc -b -v");
-// console.info(res);
-
 console.info("Rollup game");
 const buildTasks = [
     build({
